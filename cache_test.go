f450bcee905cba05fd9082b08b815e0dfa3d5abd
chaoyuepan <chaoyuepan@baidu.com>
2023-06-19 18:15:37 +0800

    upgrade and add more libs

--- cache_test.go
+++ cache_test.go
@@ -7,15 +7,21 @@ import (
 	"testing"
 	"time"
 
+	"github.com/VictoriaMetrics/fastcache"
+	akyotoCache "github.com/akyoto/cache"
 	"github.com/allegro/bigcache"
 	"github.com/bluele/gcache"
 	"github.com/coocood/freecache"
 	ristretto "github.com/dgraph-io/ristretto"
+	"github.com/erni27/imcache"
+	twoqueue "github.com/floatdrop/2q"
 	hashicorp "github.com/hashicorp/golang-lru"
+	"github.com/jellydator/ttlcache/v3"
 	koding "github.com/koding/cache"
 	"github.com/muesli/cache2go"
 	cmap "github.com/orcaman/concurrent-map"
 	cache "github.com/patrickmn/go-cache"
+	syncx "github.com/smallnest/exp/sync"
 )
 
 func toKey(i int) string {
@@ -37,26 +43,85 @@ func BenchmarkCache(b *testing.B) {
 		}
 		testStr = string(testBytes)
 
-		b.Run(fmt.Sprintf("Kodingcache-%d", n), benchmarkKodingCache)
-		b.Run(fmt.Sprintf("HashicorpLRU-%d", n), benchmarkHashicorpLRU)
-		b.Run(fmt.Sprintf("Cache2Go-%d", n), benchmarkCache2Go)
-		b.Run(fmt.Sprintf("GoCache-%d", n), benchmarkGoCache)
-		b.Run(fmt.Sprintf("Freecache-%d", n), benchmarkFreecache)
+		b.Run(fmt.Sprintf("2q-%d", n), benchmark2q)
+		b.Run(fmt.Sprintf("AkyotoCache-%d", n), BenchmarkAkyotoCache)
 		b.Run(fmt.Sprintf("BigCache-%d", n), benchmarkBigCache)
+		b.Run(fmt.Sprintf("Cache2Go-%d", n), benchmarkCache2Go)
+		b.Run(fmt.Sprintf("ConcurrentMap-%d", n), BenchmarkConcurrentMap)
+		b.Run(fmt.Sprintf("FastCache-%d", n), benchmarkFastcache)
+		b.Run(fmt.Sprintf("Freecache-%d", n), benchmarkFreecache)
+		b.Run(fmt.Sprintf("GCcache-%d", n), benchmarkGCache)
+		b.Run(fmt.Sprintf("GoCache-%d", n), benchmarkGoCache)
+		b.Run(fmt.Sprintf("Imcache-%d", n), benchmarkHashicorpLRU)
+		b.Run(fmt.Sprintf("HashicorpLRU-%d", n), benchmarkHashicorpLRU)
+		b.Run(fmt.Sprintf("Kodingcache-%d", n), benchmarkKodingCache)
 		b.Run(fmt.Sprintf("Ristretto-%d", n), benchmarkRistretto)
 		b.Run(fmt.Sprintf("SyncMap-%d", n), benchmarkSyncMap)
-		b.Run(fmt.Sprintf("ConcurrentMap-%d", n), BenchmarkConcurrentMap)
+		b.Run(fmt.Sprintf("ExpSyncMap-%d", n), benchmarkExpSyncMap)
+		b.Run(fmt.Sprintf("JellydatorTtlcache-%d", n), benchmarkJellydatorTtlcache)
+
 		fmt.Println()
 	}
 
 }
 
-func benchmarkKodingCache(b *testing.B) {
-	c := koding.NewMemoryWithTTL(time.Duration(60) * time.Second)
+func benchmark2q(b *testing.B) {
+	var m = twoqueue.New[string, string](1024 * 1024 * 5)
 
 	b.Run("Set", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			c.Set(toKey(i), testStr)
+			m.Set(toKey(i), testStr)
+		}
+	})
+
+	b.Run("Get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			value := m.Get(toKey(i))
+			_ = value
+
+		}
+	})
+}
+
+func BenchmarkAkyotoCache(b *testing.B) {
+	var m = akyotoCache.New(time.Hour)
+
+	b.Run("Set", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			m.Set(toKey(i), testStr, 0)
+		}
+	})
+
+	b.Run("Get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			value, found := m.Get(toKey(i))
+			if found {
+				_ = value
+			}
+		}
+	})
+}
+
+func benchmarkBigCache(b *testing.B) {
+	c, _ := bigcache.NewBigCache(bigcache.Config{
+		// number of shards (must be a power of 2)
+		Shards: 1024,
+		// time after which entry can be evicted
+		LifeWindow: 10 * time.Minute,
+		// rps * lifeWindow, used only in initial memory allocation
+		MaxEntriesInWindow: 1000 * 10 * 60,
+		// max entry size in bytes, used only in initial memory allocation
+		MaxEntrySize: 500,
+		// cache will not allocate more memory than this limit, value in MB
+		// if value is reached then the oldest entries can be overridden for the new ones
+		// 0 value means no size limit
+		HardMaxCacheSize: 10,
+	})
+
+	data := []byte(testStr)
+	b.Run("Set", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			c.Set(toKey(i), data)
 		}
 	})
 
@@ -64,6 +129,101 @@ func benchmarkKodingCache(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			value, err := c.Get(toKey(i))
 			if err == nil {
+				_ = value
+			}
+		}
+	})
+}
+
+func BenchmarkConcurrentMap(b *testing.B) {
+	var m = cmap.New()
+
+	b.Run("Set", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			m.Set(toKey(i), testStr)
+		}
+	})
+
+	b.Run("Get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			value, found := m.Get(toKey(i))
+			if found {
+				_ = value
+			}
+		}
+	})
+}
+
+func benchmarkCache2Go(b *testing.B) {
+	c := cache2go.Cache("test")
+
+	b.Run("Set", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			c.Add(toKey(i), 1*time.Minute, testStr)
+		}
+	})
+
+	b.Run("Get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			value, err := c.Value(toKey(i))
+			if err == nil {
+				_ = value
+			}
+		}
+	})
+}
+
+func benchmarkFastcache(b *testing.B) {
+	c := fastcache.New(1024 * 1024 * 5)
+
+	data := []byte(testStr)
+	b.Run("Set", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			c.Set([]byte(toKey(i)), data)
+		}
+	})
+
+	b.Run("Get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			value := c.Get(nil, []byte(toKey(i)))
+			_ = value
+		}
+	})
+}
+
+func benchmarkFreecache(b *testing.B) {
+	c := freecache.NewCache(1024 * 1024 * 5)
+
+	data := []byte(testStr)
+	b.Run("Set", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			c.Set([]byte(toKey(i)), data, 60)
+		}
+	})
+
+	b.Run("Get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			value, err := c.Get([]byte(toKey(i)))
+			if err == nil {
+				_ = value
+			}
+		}
+	})
+}
+
+func benchmarkGoCache(b *testing.B) {
+	c := cache.New(1*time.Minute, 5*time.Minute)
+
+	b.Run("Set", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			c.Add(toKey(i), testStr, cache.DefaultExpiration)
+		}
+	})
+
+	b.Run("Get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			value, found := c.Get(toKey(i))
+			if found {
 				_ = value
 			}
 		}
@@ -91,84 +251,32 @@ func benchmarkHashicorpLRU(b *testing.B) {
 	})
 }
 
-func benchmarkCache2Go(b *testing.B) {
-	c := cache2go.Cache("test")
+func benchmarkImcache(b *testing.B) {
+	c := imcache.New[string, string]()
 
 	b.Run("Set", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			c.Add(toKey(i), 1*time.Minute, testStr)
+			c.Set(toKey(i), testStr, imcache.WithNoExpiration())
 		}
 	})
 
 	b.Run("Get", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			value, err := c.Value(toKey(i))
-			if err == nil {
+
+			value, err := c.Get(toKey(i))
+			if err == true {
 				_ = value
 			}
 		}
 	})
 }
 
-func benchmarkGoCache(b *testing.B) {
-	c := cache.New(1*time.Minute, 5*time.Minute)
+func benchmarkKodingCache(b *testing.B) {
+	c := koding.NewMemoryWithTTL(time.Duration(60) * time.Second)
 
 	b.Run("Set", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			c.Add(toKey(i), testStr, cache.DefaultExpiration)
-		}
-	})
-
-	b.Run("Get", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			value, found := c.Get(toKey(i))
-			if found {
-				_ = value
-			}
-		}
-	})
-}
-
-func benchmarkFreecache(b *testing.B) {
-	c := freecache.NewCache(1024 * 1024 * 5)
-
-	data := []byte(testStr)
-	b.Run("Set", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			c.Set([]byte(toKey(i)), data, 60)
-		}
-	})
-
-	b.Run("Get", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			value, err := c.Get([]byte(toKey(i)))
-			if err == nil {
-				_ = value
-			}
-		}
-	})
-}
-
-func benchmarkBigCache(b *testing.B) {
-	c, _ := bigcache.NewBigCache(bigcache.Config{
-		// number of shards (must be a power of 2)
-		Shards: 1024,
-		// time after which entry can be evicted
-		LifeWindow: 10 * time.Minute,
-		// rps * lifeWindow, used only in initial memory allocation
-		MaxEntriesInWindow: 1000 * 10 * 60,
-		// max entry size in bytes, used only in initial memory allocation
-		MaxEntrySize: 500,
-		// cache will not allocate more memory than this limit, value in MB
-		// if value is reached then the oldest entries can be overridden for the new ones
-		// 0 value means no size limit
-		HardMaxCacheSize: 10,
-	})
-
-	data := []byte(testStr)
-	b.Run("Set", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			c.Set(toKey(i), data)
+			c.Set(toKey(i), testStr)
 		}
 	})
 
@@ -245,21 +353,40 @@ func benchmarkSyncMap(b *testing.B) {
 	})
 }
 
-func BenchmarkConcurrentMap(b *testing.B) {
-	var m = cmap.New()
+func benchmarkExpSyncMap(b *testing.B) {
+	var m = syncx.Map[string, string]{}
 
 	b.Run("Set", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			m.Set(toKey(i), testStr)
+			m.Store(toKey(i), testStr)
 		}
 	})
 
 	b.Run("Get", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			value, found := m.Get(toKey(i))
+			value, found := m.Load(toKey(i))
 			if found {
 				_ = value
 			}
+		}
+	})
+}
+
+func benchmarkJellydatorTtlcache(b *testing.B) {
+	var m = ttlcache.New[string, string]()
+
+	b.Run("Set", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			m.Set(toKey(i), testStr, ttlcache.NoTTL)
+		}
+	})
+
+	b.Run("Get", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			value := m.Get(toKey(i))
+
+			_ = value
+
 		}
 	})
 }
--- go.mod
+++ go.mod
@@ -1,20 +1,35 @@
 module github.com/smallnest/go-cache-benchmark
 
-go 1.13
+go 1.20
 
 require (
+	github.com/VictoriaMetrics/fastcache v1.12.1
+	github.com/akyoto/cache v1.0.6
 	github.com/allegro/bigcache v1.2.1
 	github.com/bluele/gcache v0.0.0-20190518031135-bc40bd653833
-	github.com/cespare/xxhash v1.1.0 // indirect
 	github.com/coocood/freecache v1.1.1
 	github.com/dgraph-io/ristretto v0.0.3
+	github.com/erni27/imcache v1.0.1
+	github.com/floatdrop/2q v0.1.1
 	github.com/hashicorp/golang-lru v0.5.4
 	github.com/koding/cache v0.0.0-20161222233015-e8a81b0b3f20
 	github.com/muesli/cache2go v0.0.0-20200423001931-a100c5aac93f
-	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/orcaman/concurrent-map v0.0.0-20190826125027-8c72a8bb44f6
 	github.com/patrickmn/go-cache v2.1.0+incompatible
-	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/smallnest/exp v0.0.0-20230619045857-65ada312c447
+)
+
+require (
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/floatdrop/fifo v0.1.1 // indirect
+	github.com/floatdrop/lru v1.2.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/jellydator/ttlcache/v3 v3.0.1 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.5.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
